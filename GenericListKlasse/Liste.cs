27215aca6eef77032cd29e10a2368d613a297abf
Kevin Feilen <kvnfeilen28@gmail.com>
2018-09-10 03:47:09 +0200

    Initialstand

---
+++ GenericListKlasse/Liste.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericListKlasse
+{
+    public class Liste<T> 
+    {
+        private T[] _liste;
+        public int Count { get; private set; }
+
+        public Liste(int count = 0)
+        {
+            _liste = new T[count];
+            Count = count;
+        }
+
+        public T this[int index] { get { return _liste[index]; } set { _liste[index] = value; } }
+
+
+        public void Add(T item) //tested
+        {
+            if (item.GetType() == typeof(T))
+            {
+                try
+                {
+                    T[] newList = new T[this.Count + 1];
+                    _liste.CopyTo(newList,0);
+                    newList[this.Count] = item;
+                    _liste = newList;
+                    this.Count = _liste.Count();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        
+        public void AddRange(IEnumerable<T> collection)  //getestet
+        {
+            try
+            {
+                T[] newList = new T[this.Count + collection.Count()];
+                _liste.CopyTo(newList, 0);
+                int i = this.Count;
+                foreach (var item in collection)
+                {
+                    newList[i] = item;
+                    i++;
+                }
+                _liste = newList;
+                this.Count = _liste.Count();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void RemoveRange(int index, int count)  //getestet
+        {
+            try
+            {
+                T[] newList = new T[this.Count - count];
+                for (int i = 0; i < index; i++)
+                {
+                    newList[i] = _liste[i];
+                }
+                for (int i = index; i <= newList.Count() -1; i++)
+                {
+                    newList[i] = _liste[i+count];
+                }
+                _liste = newList;
+                this.Count= _liste.Count();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void RemoveAt(int index)  //getestet
+        {
+            try
+            {
+                T[] listWithoutItem = new T[this.Count - 1];
+                int i = -1;
+                int indexCleaner = 0;
+                foreach (var item in _liste)
+                {
+                    i++;
+                    if (i == index) 
+                    {
+                        indexCleaner++;
+                        continue;
+                    }
+                    listWithoutItem[i-indexCleaner] = item;
+                }
+                _liste = listWithoutItem;
+                this.Count = _liste.Count();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void Reverse()  //tested
+        {
+            try
+            {
+                T[] listReversed = new T[this.Count];
+                int i = this.Count - 1;
+                foreach (var item in _liste)
+                {
+                    listReversed[i] = item;
+                    i--;
+                }
+                _liste = listReversed;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void Clear()  //tested
+        {
+            try
+            {
+                _liste = new T[0];
+                this.Count = _liste.Count();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public T FindLast(T match)
+        {
+            if (match == null)
+            {
+                throw new ArgumentNullException("match", "The \"match\" Parameter is null");
+            }
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                if (_liste[i].Equals(match))
+                {
+                    return _liste[i];
+                }
+            }
+            return (T)(Activator.CreateInstance(typeof(T)));
+
+        }
+
+        public int FindLastIndex(T match)
+        {
+            if (match == null)
+            {
+                throw new ArgumentNullException("match", "The \"match\" Parameter is null");
+            }
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                if (_liste[i].Equals(match))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public T Find(T match)
+        {
+            if (match == null)
+            {
+                throw new ArgumentNullException("match", "The \"match\" Parameter is null");
+            }
+            for (int i = 0; i <= this.Count - 1; i++)
+            {
+                if (_liste[i].Equals(match))
+                {
+                    return _liste[i];
+                }
+            }
+            return (T)(Activator.CreateInstance(typeof(T)));
+
+        }
+
+        public int FindIndex(T match)
+        {
+            if (match == null)
+            {
+                throw new ArgumentNullException("match", "The \"match\" Parameter is null");
+            }
+            for (int i = 0; i <= this.Count - 1; i++)
+            {
+                if (_liste[i].Equals(match))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(_liste);
+        }
+
+        public struct Enumerator : IDisposable
+        {
+            public Enumerator( T[] liste)
+            {
+                _liste = liste;
+                index = -1;
+                Count = _liste.Count();
+            }
+            private int index { get; set; }
+            private int Count { get; set; }
+            private T[] _liste { get; set; }
+            public T Current => _liste[index];
+
+            public void Dispose()
+            {
+
+            }
+
+            public bool MoveNext()
+            {
+                if (index+1 < Count)
+                {
+                    index++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
--- GenericListKlasse/Program.cs
+++ GenericListKlasse/Program.cs
@@ -10,6 +10,92 @@ namespace GenericListKlasse
     {
         static void Main(string[] args)
         {
+            //<BEGIN> Befüllen
+            List<string> testliste = new List<string>();
+            testliste.Add("hallo");
+            testliste.Add("welt");
+            Liste<string> liste = new Liste<string>();
+            liste.Add("hallo");
+            liste.Add("welt");
+            
+            for (int i = 0; i <= 20; i++)
+            {
+                testliste.Add(i.ToString());
+                liste.Add(i.ToString());
+            }
+            //<END> Befüllen
+            printAuswertungParallel(liste, testliste);
+
+            //<BEGIN> Reverse
+            Console.WriteLine("******************Reverse***************");
+            liste.Reverse();
+            testliste.Reverse();
+            printAuswertungParallel(liste, testliste);
+
+            //<END> Reverse
+
+            //<BEGIN> RemoveAt
+            Console.WriteLine("*************RemoveAt**************");
+
+            liste.RemoveAt(12);
+            testliste.RemoveAt(12);
+
+            printAuswertungParallel(liste, testliste);
+
+            //<END> RemoveAt
+
+            //<BEGIN> RemoveRange
+            Console.WriteLine("*************RemoveRange*************");
+            liste.RemoveRange(5, 2);
+            testliste.RemoveRange(5, 2);
+            printAuswertungParallel(liste, testliste);
+
+            //<END> RemoveRange
+
+            //<BEGIN> AddRange
+            Console.WriteLine("**************AddRange**************");
+            List<string> range = new List<string>();
+            for(int i = 1; i < 10; i++)
+            {
+                range.Add((i * 100).ToString());
+            }
+            liste.AddRange(range);
+            testliste.AddRange(range);
+            printAuswertungParallel(liste, testliste);
+
+            //<END> AddRange
+            liste.AddRange(range);
+            testliste.AddRange(range);
+
+            printAuswertungParallel(liste, testliste);
+
+            Console.ReadKey();
+        }
+
+
+        static public void printAuswertung(Liste<string> ownList, List<string> stdList)
+        {
+            Console.WriteLine("************Eigene Liste*************");
+            foreach (var item in ownList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("************Normale Liste*************");
+            foreach (var item in stdList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static public void printAuswertungParallel(Liste<string> ownList, List<string> stdList)
+        {
+            Console.WriteLine("*\tEigene*****Original*");
+            int i = 0;
+            foreach (var item in ownList)
+            {
+                Console.WriteLine($"\t{item}*****{stdList[i]}");
+                i++;
+            }
         }
     }
 }
